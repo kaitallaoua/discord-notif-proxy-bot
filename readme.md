81a1c9c603fd7e5c0c80fee43b7bde4551d841d1
Karim Ait-Allaoua <kaitallaoua@gmail.com>
2022-08-21 20:16:35 -0600

    Upgrade install to be consistent, add to readme

---
+++ readme.md
@@ -0,0 +1,16 @@
+# discord-notif-proxy-bot
+Send discord webhook messages reporting on proxy server status.
+
+Tested on python 3.10.4 on windows.
+
+Install Instructions:
+
+1. Install python 3.10
+
+2. clone this repo
+
+3. cd into the repo
+
+4. run 'setup.ps1'
+
+5. run 'run.ps1' to start the script. Consider scheduling it with something like task scheduler.
--- run.ps1
+++ run.ps1
@@ -1,9 +1,15 @@
-param([string]$wd="")
+param([string]$wd = "")
 
-Write-Output "Sleeping"
+Write-Output "Sleeping to let things initialize..."
 Start-Sleep -Seconds 30.0
 Write-Output "Done Sleep"
+
 Set-Location $wd
-.\.venv\Scripts\Activate.ps1
+
 Write-Output "Init virtual enviroment"
+.\.venv\Scripts\Activate.ps1
+
+Write-Output "Starting Script"
 python .\proxy_notif_bot.py
+
+Write-Output "Done"
